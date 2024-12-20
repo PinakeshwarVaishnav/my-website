29616ca79c69fac73137a80103735cdc9e512fd5
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-12-20 19:52:35 +0530

    update app page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 export default function App() {
   return (
     <div>
-      <h1>Hi, I am Pinakeshwar Vaishnav</h1>
+      <h1>🚀 Full Stack Developer & AI/ML Enthusiast </h1>
+      <p>
+        Hi there! I&apos;m Pinakeshwar Vaishnav and I&apos;m a Full Stack
+        Developer. I&apos;m also passionate about Artificial Intelligence and
+        Machine Learning, with a strong interest in exploring their applications
+        in real-world projects.
+      </p>
     </div>
   );
 }
