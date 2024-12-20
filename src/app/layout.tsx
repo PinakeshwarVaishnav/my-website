85163286477f087f35bdf60cdccfdf8554b6ebb5
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2024-12-20 19:11:39 +0530

    feat: add dark mode in the app

---
+++ src/app/layout.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { ThemeProvider } from "next-themes";
+
+export default function AppLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-export default function HomePage() {
+export default function App() {
   return (
     <div>
       <h1>Hi, I am Pinakeshwar Vaishnav</h1>
