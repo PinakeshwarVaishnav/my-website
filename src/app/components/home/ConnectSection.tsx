fc0127bb03fb164ca03e436e83248783d02965a1
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2025-02-20 14:10:07 +0530

    Add labels below icons

--- src/app/components/home/ConnectSection.tsx
+++ src/app/components/home/ConnectSection.tsx
@@ -4,24 +4,33 @@ export default function ConnectSection() {
   return (
     <section>
       <div className="flex justify-center items-center space-x-8 mt-10">
-        <a href="https://github.com/PinakeshwarVaishnav">
-          <FaGithub
-            size={40}
-            className="transform transition-transform duration-300 hover:scale-110"
-          />
-        </a>
-        <a href="https://www.linkedin.com/in/pinakeshwar-vaishnav-519750306/">
-          <FaLinkedin
-            size={40}
-            className="transform transition-transform duration-300 hover:scale-110"
-          />
-        </a>
-        <a href="https://twitter.com/PinakeshwarV">
-          <FaXTwitter
-            size={40}
-            className="transform transition-transform duration-300 hover:scale-110"
-          />
-        </a>
+        <div className="flex flex-col justify-center items-center">
+          <a href="https://github.com/PinakeshwarVaishnav">
+            <FaGithub
+              size={40}
+              className="transform transition-transform duration-300 hover:scale-110"
+            />
+            <span className="mt-3">GitHub</span>
+          </a>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <a href="https://www.linkedin.com/in/pinakeshwar-vaishnav-519750306/">
+            <FaLinkedin
+              size={40}
+              className="transform transition-transform duration-300 hover:scale-110"
+            />
+            <span className="mt-3">LinkedIn</span>
+          </a>
+        </div>
+        <div className="flex flex-col justify-center items-center">
+          <a href="https://twitter.com/PinakeshwarV">
+            <FaXTwitter
+              size={40}
+              className="transform transition-transform duration-300 hover:scale-110"
+            />
+            <span className="mt-3">X</span>
+          </a>
+        </div>
       </div>
     </section>
   );
