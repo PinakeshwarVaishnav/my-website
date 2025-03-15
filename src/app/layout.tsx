871eca692c74c0f66286ea36644dd6acdf961d95
PinakeshwarVaishnav <122798447+PinakeshwarVaishnav@users.noreply.github.com>
2025-03-15 20:07:24 +0530

    remove navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,6 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className="antialiased flex flex-col items-center justify-center mx-auto mt-4 lg:mt-8 mb-20 lg:mb-40 bg-gray-900 text-white bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-900/20 via-gray-900 to-gray-900">
-        <NavBar />
         {children}
       </body>
     </html>
